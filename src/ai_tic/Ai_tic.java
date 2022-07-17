560ccb5c2228cdc95984624f97cdf701d179364c
ruyaatatreh99 <ruyaatatreh99@gmail.com>
2022-07-17 12:51:35 +0300

    AI

---
+++ src/ai_tic/Ai_tic.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ai_tic;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Ai_tic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
